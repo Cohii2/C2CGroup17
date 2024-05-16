21e894922b7b9edf4e34a636a1c0caaa1d96e390
Cohii <hengjunxiangg@gmail.com>
2024-05-16 10:33:08 +0800

    Implement dataclass decorator
    Implemented @dataclass for the Order class to make code cleaner.

--- matching_engine/order.py
+++ matching_engine/order.py
@@ -1,5 +1,7 @@
+from dataclasses import dataclass, field
 import datetime
 
+@dataclass
 class Order:
     """Object representing an Order in an Order Book.
 
@@ -9,27 +11,16 @@ class Order:
         price (float): A float indicating the price per unit of the order. 
         time (datetime.datetime): A datetime object indicating the time the order was placed. 
     """
-
-    def __init__(self, id : str, quantity : int, price : float, time : datetime.datetime = None):
-        """
-        Initializes an Order object.
-
-        Args:
-            id (str): The serial number of the order.
-            quantity (int): The quantity of the order.
-            price (float): The price per unit of the order.
-            time (datetime.datetime): The time the order was placed. If not provided, the current time will be used.
-        """
-
-        self.id = id
-        self.quantity = quantity 
-        self.price = price
-        self.time = time if time is not None else datetime.datetime.now()
+    id : str
+    quantity : int 
+    price : float 
+    time : datetime.datetime = field(default_factory = datetime.datetime.now)
     
     def __str__(self):
         """Returns a string representation of the Order object."""
         return f"{self.__class__.__name__} | ID: {self.id} | Quantity: {self.quantity} | Price: {self.price} | Time: {self.time}"
 
+@dataclass
 class MarketOrder(Order):
     """Object representing a Market Order in an Order Book.
 
@@ -40,20 +31,9 @@ class MarketOrder(Order):
         quantity (int): An integer indicating the quantity of the order.
         time (datetime.datetime): A datetime object indicating the time the order was placed. 
     """
+    price : float = field(default = 0.0)
 
-    """Default price used"""
-    DEFAULT_PRICE = 0.0
-
-    def __init__(self, id : str, quantity : int, time : datetime.datetime = None):
-        """Initialises a Market Order object.
-        
-        Args:
-            id (str): The serial number of the order.
-            quantity (int): The quantity of the order.
-            time (datetime.datetime): The time the order was placed. If not provided, the current time will be used.
-        """
-        super().__init__(id = id, quantity = quantity, price = MarketOrder.DEFAULT_PRICE, time = time)
-
+@dataclass
 class LimitOrder(Order):
     """Object representing a Limit Order in an Order Book.
 
@@ -65,15 +45,3 @@ class LimitOrder(Order):
         price (float): A float indicating the price per unit of the order. 
         time (datetime.datetime): A datetime object indicating the time the order was placed. 
     """
-
-    def __init__(self, id : str, quantity : int, price : float, time : datetime.datetime = None):
-        """
-        Initialises a Limit Order object.
-        
-        Args:
-            id (str): The serial number of the order.
-            quantity (int): The quantity of the order.
-            price (float): The price per unit of the order.
-            time (datetime.datetime): The time the order was placed. If not provided, the current time will be used.
-        """
-        super().__init__(id = id, quantity = quantity, price = price, time = time)
